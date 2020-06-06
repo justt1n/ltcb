c5a4151cd00df9f8f56ba7ce8014521ddb522d7e
t1n <t1n@pop-os.localdomain>
2020-06-06 15:20:29 +0700

    change

--- tapTin3.c
+++ tapTin3.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int countLines(FILE *a)
+{
+    char ch[100];
+    int count = 0;
+    while (fgets(ch, sizeof(ch), a) != NULL)
+    {
+        count++;
+    }
+    return count;
+}
 
 int main()
 {
-    char a[100];
-    fgets(a, 100, stdin);
-    int i = 0;
-    while (a[i] != '\0')
-    {
-        if (a[i] == '\n')
-        {
-            a[i] = '\0';
-            break;
-        }
-        i++;
-    }
-    FILE *file = fopen(a, "r");
-    int count = 0;
-    while
+    char name[40];
+    scanf("%s", name);
+    FILE *file = fopen(name, "r");
+    printf("%d", countLines(file));
+    fclose(file);
+    return 0;
 }
